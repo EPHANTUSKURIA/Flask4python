2451095805a1563cc7db7a9d77a7af3165e08b19
Kuria Maina <90629343+EPHANTUSKURIA@users.noreply.github.com>
2022-11-15 23:12:41 +0300

    created test db and fixed an error

--- app.py
+++ app.py
@@ -8,16 +8,15 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    content = db.Column(db.String(200),nullable=False)
+    content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id 
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
-    
     return render_template('index.html')
 
 if __name__=="__main__":
